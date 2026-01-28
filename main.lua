25b32e1cb503deee42b4ba3809886a378132d07e
RoFearHypno <fearhypno@gmail.com>
2026-01-28 14:56:29 -0500

    Update main.lua
    No longer trusts PACKETIDS.

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@
 
 -- Script is limited to a maximum of 255 specific packets. Try to send to any higher will fail.
 -- This script does trust PacketId's, so be careful. (Meaning expecting 0-255 and not checking)
+-- SCRIPT NO LONGER TRUSTS PACKETIDS
 
 -- / Services \ --
 local HttpService = game:GetService("HttpService")
@@ -22,6 +23,8 @@ export type BW = {
 	string: (PacketId: number, Str: string) -> (buffer | nil),
 	u8: (PacketId: number, Value: number) -> (buffer | nil),
 	u16: (PacketId: number, Value: number) -> (buffer | nil),
+	i8: (PacketId: number, Value: number) -> (buffer | nil),
+	i16: (PacketId: number, Value: number) -> (buffer | nil),
 }
 -- \       / --
 
@@ -44,7 +47,7 @@ local function CheckLimits(BitSize: number, Signed: boolean, Value: number): (bo
 		LowerLimit = -2 ^ (BitSize)
 		HigherLimit = LowerLimit * -1 - 1
 	else
-		HigherLimit = 2 ^ BitSize -1
+		HigherLimit = 2 ^ BitSize - 1
 	end
 	
 	if Value < LowerLimit or Value > HigherLimit then
@@ -59,70 +62,102 @@ local function CheckLimits(BitSize: number, Signed: boolean, Value: number): (bo
 	return true, ""
 end
 
+-- Local function the type checks the PacketId and CallerName and then returns a buffer with the flag and packet id
+-- Limitation: 0-255 Packet ID | CallerName: 0-(64b limit)
+local function WriteFlagAndPacketId(PacketId: number, CallerName: string, Size: number) : (buffer | nil)
+	assert(CallerName, "[ByteWriter.WriteFlagAndPacketId(lf)]: ⚠️ CallerName arg wasn't provided or resulted to nil!")
+	assert(typeof(CallerName) == "string", "[ByteWriter.WriteFlagAndPacketId(lf)]: ⚠️ CallerName arg wasn't nil, but isn't type 'string'!")
+	
+	assert(Size, "[ByteWriter.WriteFlagAndPacketId(lf)]: ⚠️ Size arg wasn't provided or resulted to nil!")
+	assert(typeof(Size) == "number", "[ByteWriter.WriteFlagAndPacketId(lf)]: ⚠️ Size arg wasn't nil, but isn't type 'number'!")
+	if Size < 2 then return nil end
+	
+	if not PacketId then warn(`[ByteWriter.{CallerName}]: ⚠️ PacketId arg wasn't provided or resulted to nil!`) return nil end
+	if typeof(PacketId) ~= "number" then warn(`[ByteWriter.{CallerName}]: ⚠️ PacketId arg wasn't nil, but isn't type 'number'!`) return nil end
+	if PacketId < 0 or PacketId > 255 then return nil end
+	
+	local Buff = buffer.create(Size)
+	buffer.writeu8(Buff, 0, 1)
+	buffer.writeu8(Buff, 1, PacketId)
+	-- [Flag] [PacketId]
+	
+	return Buff
+end
+
 -- Limitations:  0-255   (8-bit Unsigned Integer)
 function BufferWriter.u8(PacketId: number, Value: number): buffer | nil
-	if PacketId == nil then return nil end
-	
 	local Check, Err = CheckLimits(8, false, Value)
 	if not Check then warn(Err) return nil end
 	
-	local Buff = buffer.create(3)
+	local Buff: (buffer | nil) = WriteFlagAndPacketId(PacketId, "u8", 3)
+	if Buff == nil then return nil end
 	--flag + id + value  (1 each)
 
-	local Cursor = 0
-	buffer.writeu8(Buff, Cursor, 1)
-	Cursor += 1
-
-	buffer.writeu8(Buff, Cursor, PacketId)
-	Cursor += 1
-
-	buffer.writeu8(Buff, Cursor, Value)
+	--local Cursor = 2
+	buffer.writeu8(Buff, 2, Value)
 
 	return Buff
 end
 
 -- Limitations:  0-65535   (16-bit Unsigned Integer)
 function BufferWriter.u16(PacketId: number, Value: number): buffer | nil
-	if PacketId == nil then return nil end
-
 	local Check, Err = CheckLimits(16, false, Value)
 	if not Check then warn(Err) return nil end
-
-	local Buff = buffer.create(4)
+	
+	local Buff: (buffer | nil) = WriteFlagAndPacketId(PacketId, "u16", 4)
+	if Buff == nil then return nil end
 	--flag + id + (2)value  (1 each)
 
-	local Cursor = 0
-	buffer.writeu8(Buff, Cursor, 1)
-	Cursor += 1
+	--local Cursor = 2
+	buffer.writeu16(Buff, 2, Value)
 
-	buffer.writeu8(Buff, Cursor, PacketId)
-	Cursor += 1
+	return Buff
+end
 
-	buffer.writeu16(Buff, Cursor, Value)
+-- Limitations:  -128 - 127   (8-bit signed Integer)
+function BufferWriter.i8(PacketId: number, Value: number): buffer | nil
+	local Check, Err = CheckLimits(8, true, Value)
+	if not Check then warn(Err) return nil end
+
+	local Buff: (buffer | nil) = WriteFlagAndPacketId(PacketId, "i8", 3)
+	if Buff == nil then return nil end
+	--flag + id + value  (1 each)
+
+	--local Cursor = 2
+
+	buffer.writei8(Buff, 2, Value)
+
+	return Buff
+end
+
+-- Limitations:  -32768 - 32767   (16-bit signed Integer)
+function BufferWriter.i16(PacketId: number, Value: number): buffer | nil
+	local Check, Err = CheckLimits(16, true, Value)
+	if not Check then warn(Err) return nil end
+	
+	local Buff: (buffer | nil) = WriteFlagAndPacketId(PacketId, "i16", 4)
+	if Buff == nil then return nil end
+	--flag + id + (2)value  (1 each)
+
+	--local Cursor = 2
+	buffer.writei16(Buff, 2, Value)
 
 	return Buff
 end
 
 -- String Character Limitations:  0-65535  (16-bit Unsigned Integer)
 function BufferWriter.string(PacketId: number, Str: string): buffer | nil
-	if PacketId == nil then return nil end
-	
 	local StringLength = #Str
 	
 	local Check, Err = CheckLimits(16, false, StringLength)
 	if not Check then warn(Err) return nil end
 
-	local Buff = buffer.create(StringLength + 4)
+	local Buff: (buffer | nil) = WriteFlagAndPacketId(PacketId, "i16", StringLength + 4)
+	if Buff == nil then return nil end
 	-- flag + id + value + StringLength
 	-- 1+1+1+#Str
 	
-	local Cursor = 0
-
-	buffer.writeu8(Buff, Cursor, 1)
-	Cursor += 1
-
-	buffer.writeu8(Buff, Cursor, PacketId)
-	Cursor += 1
+	local Cursor = 2
 
 	buffer.writeu16(Buff, Cursor, StringLength)
 	Cursor += 2
@@ -135,8 +170,8 @@ end
 -- Trusts that Remote is provided.
 -- Doesn't trust that the remote is correct.
 function BufferWriter.send(Buff: buffer, Remote: string)
-	if Buff == nil then return end
-	if typeof(Buff) ~= "buffer" then warn("[BufferWriter.send]: ⚠️ Buff arg isn't nil, but isn't type 'bufffer'!") return end
+	if Buff == nil then warn("[BufferWriter.send]: ⚠️ Buff arg is nil or resulted to nil!") return end
+	if typeof(Buff) ~= "buffer" then warn("[BufferWriter.send]: ⚠️ Buff arg isn't nil, but isn't type 'buffer'!") return end
 	
 	local RemoteEvent = Remote == "Reliable" and Reliable or Remote == "Unreliable" and Unreliable
 	if RemoteEvent == nil then warn("[BufferWriter.send]: ⚠️ Remote arg was provided, but isn't a valid remote type!") return end
